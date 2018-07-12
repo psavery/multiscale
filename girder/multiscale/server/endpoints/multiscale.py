76e3338c3404d91d1e8edfabd433c223090f24e9
Patrick Avery <psavery@buffalo.edu>
2018-07-12 14:50:35 -0400

    Added an api interface for smtk job submission

--- girder/multiscale/server/endpoints/multiscale.py
+++ girder/multiscale/server/endpoints/multiscale.py
@@ -18,6 +18,7 @@ from girder_worker.docker.transforms.girder import (
 from girder.plugins.jobs.models.job import Job
 
 ALBANY_IMAGE = 'openchemistry/albany'
+SMTK_IMAGE = 'psavery/smtk'
 
 
 class MultiscaleEndpoints(Resource):
@@ -28,6 +29,8 @@ class MultiscaleEndpoints(Resource):
         super(MultiscaleEndpoints, self).__init__()
         self.route('POST', ('run_albany', ),
                    self.run_albany)
+        self.route('POST', ('run_smtk_mesh_placement', ),
+                   self.run_smtk_mesh_placement)
 
     @access.token
     @filtermodel(model=Job)
@@ -58,6 +61,53 @@ class MultiscaleEndpoints(Resource):
         result = docker_run.delay(
             ALBANY_IMAGE, pull_image=False, container_args=[filename],
             entrypoint='/usr/local/albany/bin/AlbanyT', remove_container=True,
+            working_dir=volume,
+            girder_result_hooks=[
+                GirderUploadVolumePathToFolder(volumepath, outputFolderId)
+            ])
+
+        # We want to update the job with some multiscale settings.
+        # We will put it in the meta data.
+        job = Job().findOne({'_id': result.job['_id']})
+        multiscale_io = {
+            'meta': {
+                'multiscale_settings': {
+                    'inputFolderId': inputFolderId,
+                    'outputFolderId': outputFolderId
+                }
+            }
+        }
+
+        return Job().updateJob(job, otherFields=multiscale_io)
+
+    @access.token
+    @filtermodel(model=Job)
+    @autoDescribeRoute(
+        Description('Run smtk mesh placement from a girder folder')
+        .param('inputFolderId', 'The id of the input folder on girder.'
+               '"input.json" must be inside, along with any other '
+               'necessary input files. Will store the output in the '
+               'specified output folder.',
+               paramType='query', dataType='string', required='True')
+        .param('outputFolderId', 'The id of the output folder on girder.',
+               paramType='query', dataType='string', required='True'))
+    def run_smtk_mesh_placement(self, params):
+        """Run an smtk mesh placement on a folder that is on girder.
+
+        Will store the output in the specified output folder.
+        """
+        inputFolderId = params.get('inputFolderId')
+        outputFolderId = params.get('outputFolderId')
+        folder_name = 'workingDir'
+        volume = GirderFolderIdToVolume(
+            inputFolderId,
+            volume=TemporaryVolume.default,
+            folder_name=folder_name)
+        outputDir = inputFolderId + '/' + folder_name + '/'
+        volumepath = VolumePath(outputDir, volume=TemporaryVolume.default)
+        result = docker_run.delay(
+            SMTK_IMAGE, pull_image=False, container_args=["-c", ". ~/setupEnvironment; python /usr/local/afrl-automation/runner.py input.json"],
+            entrypoint='bash', remove_container=True,
             working_dir=volume,
             girder_result_hooks=[
                 GirderUploadVolumePathToFolder(volumepath, outputFolderId)
